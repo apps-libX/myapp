ecba45b50961a19d6b14412b53c0d43d0ad9a76f
anonymoussc <50c5ac69@opayq.com>
2015-11-09 12:27:18 +0700

    add namespaces..unused file.

--- angular/dashboard/layout/layout.js
+++ angular/dashboard/layout/layout.js
@@ -1,7 +1,7 @@
 (function(){
     "use strict";
 
-    angular.module('app.controllers').controller('LayoutCtrl', function(){
+    angular.module('app.controllers').controller('Dashboard.LayoutCtrl', function(){
         //
     });
 
--- angular/staticpage/layout/layout.js
+++ angular/staticpage/layout/layout.js
@@ -1,7 +1,7 @@
 (function(){
     "use strict";
 
-    angular.module('app.controllers').controller('LayoutCtrl', function(){
+    angular.module('app.controllers').controller('Staticpage.LayoutCtrl', function(){
         //
     });
 
